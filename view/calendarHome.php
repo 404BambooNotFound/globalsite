ca22c84c627fe093e43b656f4ad126825c97b280
alexianeuf <neufond.alexia@gmail.com>
2018-12-07 06:58:20 +0100

    modif

--- view/calendarHome.php
+++ view/calendarHome.php
@@ -5,14 +5,18 @@ include_once("header.php");
 <div class="row">
     <img class="col-md-3 side_bamboos" src="../img/side_bamboo.png">
     <div class="col-md-6">
-        <h1>GIF du mois de
+
+
             <?php
             $today = getdate();
             $img_url = "/img/gif/".strftime("%B%Y").".gif";
             setlocale(LC_TIME, 'fr_FR.utf-8', 'fra');
-            echo iconv("ISO-8859-1", "UTF-8", strftime("%B")); ?></h1>
+            $month = iconv("ISO-8859-1", "UTF-8", strftime("%B"));?>
+            b<h1><?php echo "Aujourd'hui, nous sommes le ". iconv("ISO-8859-1", "UTF-8", strftime("%d %B %Y")); ?></h1>
+            <h1>GIF du mois de <?php echo $month ?></h1>
 
-            <?php echo '<img src="'.$img_url.'">';?>
+
+            <center><?php echo '<img src="'.$img_url.'">';?></center>
         <center ><button class="previous" id="btn-previous" onclick="seePreviousGIF()">Redécouvrez les GIFs des mois précédents</button></center>
     </div>
     <img class="col-md-3 side_bamboos" src="../img/side_bamboo_revert.png">
